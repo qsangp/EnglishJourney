0cbdf5c346353f0be4d7c7e07d5935417d6c4972
qsangp <ieltsvuive@gmail.com>
2021-05-26 18:27:40 +0700

    add label and button

---
+++ EnglishJourney_2/CardLessonVC.swift
@@ -0,0 +1,46 @@
+//
+//  CardLessonVC.swift
+//  EnglishJourney_2
+//
+//  Created by ielts-vuive on 26/05/2021.
+//
+
+import UIKit
+
+class CardLessonVC: UIViewController {
+    @IBOutlet weak var lessonLabel: UILabel!
+    @IBOutlet weak var audioFrontLabel: UIButton!
+    @IBOutlet weak var questionFrontLabel: UILabel!
+    @IBOutlet weak var showHideButtonLabel: UIButton!
+    
+    @IBOutlet weak var audioBackLabel: UIButton!
+    @IBOutlet weak var textBackField: UITextView!
+    
+    @IBOutlet weak var againButtonLabel: UIButton!
+    @IBOutlet weak var completeButtonLabel: UIButton!
+    @IBOutlet weak var backToLessonButtonLabel: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func audioFrontPressed(_ sender: UIButton) {
+    }
+    @IBAction func audioBackPressed(_ sender: UIButton) {
+    }
+    @IBAction func showHideButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func againButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func completeButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func backToLessonButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+
+
+}
