bb55b793379c5ce95fc2c084867270f8693ca17d
ielts-vuive <macbook@ielts-vuive.local>
2021-05-26 14:39:09 +0700

    add layout lesson

---
+++ EnglishJourney_2/CardDataVC.swift
@@ -0,0 +1,29 @@
+//
+//  CardDataVC.swift
+//  EnglishJourney_2
+//
+//  Created by ielts-vuive on 26/05/2021.
+//
+
+import UIKit
+
+class CardDataVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
